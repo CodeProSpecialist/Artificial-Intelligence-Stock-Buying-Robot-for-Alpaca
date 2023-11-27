6a6a3e357a688adc6c6c05796045dbc4d81b1f20
codeprospecialist <droark28@hotmail.com>
2023-11-27 14:50:46 -0600

    Code bugs were fixed.
    Signed-off-by: codeprospecialist <droark28@hotmail.com>

--- GPT-Artificial-Intelligence-stock-buying-robot.py
+++ GPT-Artificial-Intelligence-stock-buying-robot.py
@@ -14,10 +14,12 @@ APIBASEURL = os.getenv('APCA_API_BASE_URL')
 # Initialize the Alpaca API
 api = tradeapi.REST(APIKEYID, APISECRETKEY, APIBASEURL)
 
+
 # Function to get historical price data using yfinance
 def get_historical_data(symbol, start_date, end_date):
     stock_data = yf.download(symbol, start=start_date, end=end_date)
     return stock_data
+
 
 # Function to perform sentiment analysis using transformers library
 def analyze_sentiment(text):
@@ -25,22 +27,26 @@ def analyze_sentiment(text):
     result = sentiment_analyzer(text)
     return result[0]['label']
 
+
 # Function to generate GPT-based internet searches
 def generate_internet_search(query, role_instruction):
     gpt_search_generator = pipeline('text-generation', model='EleutherAI/gpt-neo-1.3B')
     print("Searching the internet for stock symbols...\n")
-    search_result = gpt_search_generator(role_instruction + "\n" + query, max_length=150, num_return_sequences=1, temperature=0.7)
+    search_result = gpt_search_generator(role_instruction + "\n" + query, max_length=150, num_return_sequences=1,
+                                         temperature=0.7)
     return search_result[0]['generated_text']
+
 
 # Function to get the price percentage change over the past two days
 def get_price_change_percentage(symbol):
     stock_info = yf.Ticker(symbol)
     historical_data = stock_info.history(period='2d')['Close']
-    
+
     # Calculate percentage change
     price_change_percentage = (historical_data.iloc[-1] - historical_data.iloc[0]) / historical_data.iloc[0] * 100
-    
+
     return price_change_percentage
+
 
 # Function to process GPT-generated search results, extract symbols, and filter by price change
 def filter_symbols_by_price_change(search_result, percentage_threshold=1.0):
@@ -48,9 +54,11 @@ def filter_symbols_by_price_change(search_result, percentage_threshold=1.0):
     relevant_symbols = extract_symbols_from_gpt_result(search_result)
 
     # Filter symbols by price change percentage
-    symbols_with_increase = [symbol for symbol in relevant_symbols if get_price_change_percentage(symbol) >= percentage_threshold]
+    symbols_with_increase = [symbol for symbol in relevant_symbols if
+                             get_price_change_percentage(symbol) >= percentage_threshold]
 
     return symbols_with_increase
+
 
 # Function to extract symbols from GPT search result
 def extract_symbols_from_gpt_result(search_result):
@@ -64,22 +72,26 @@ def extract_symbols_from_gpt_result(search_result):
 
     return relevant_symbols
 
+
 # Function to check if there's enough cash in the Alpaca account
 def has_enough_cash(cash_required):
     account_info = api.get_account()
     cash_available = float(account_info.cash)
     return cash_available >= cash_required
 
+
 # Function to check if the stock price is within the budget
 def is_price_within_budget(symbol, budget):
     current_price = get_current_price(symbol)
     return current_price <= budget
+
 
 # Function to get the current price of a stock symbol
 def get_current_price(symbol):
     stock_info = yf.Ticker(symbol)
     current_price = stock_info.history(period='1d')['Close'].iloc[-1]
     return current_price
+
 
 # Function to check if the current time is within stock market hours (Eastern Time)
 def is_market_open():
@@ -90,13 +102,15 @@ def is_market_open():
 
     return market_open <= current_time <= market_close
 
+
 # Main trading function
 def main():
     while True:
         try:
             # Check if the market is open during Eastern Time
             if not is_market_open():
-                print("This stock trading robot only works during stock market hours. Waiting for the stock market to open for trading….")
+                print(
+                    "This stock trading robot only works during stock market hours. Waiting for the stock market to open for trading….")
                 time.sleep(60)
                 continue
 
@@ -107,11 +121,13 @@ def main():
 
             # Generate a query for GPT-based internet search
             gpt_search_query = "strong buy ETF fund stocks USA MarketWatch"
-            role_instruction = "Role: Searching the internet for strong buy ETF funds or stocks\n" 
+            role_instruction = "Role: Searching the internet for strong buy ETF funds or stocks\n"
+
+            print("Searching the internet for successful ETF funds or stocks to purchase with the GPT Artificial Intelligence robot.....")
 
             # Get the generated search result
             gpt_search_result = generate_internet_search(gpt_search_query, role_instruction)
-            
+
             # Extract and print all stock symbols
             print("List of Stock Symbols Extracted:")
             symbols_from_gpt = extract_symbols_from_gpt_result(gpt_search_result)
@@ -129,7 +145,8 @@ def main():
             for symbol in symbols_with_increase:
                 current_price = get_current_price(symbol)
                 price_change_percentage = get_price_change_percentage(symbol)
-                print(f"Symbol: {symbol}, Current Price: {current_price:.2f}, Percentage Change: {price_change_percentage:.2f}%")
+                print(
+                    f"Symbol: {symbol}, Current Price: {current_price:.2f}, Percentage Change: {price_change_percentage:.2f}%")
 
             # Check if there are symbols with an increase
             if symbols_with_increase:
@@ -156,6 +173,7 @@ def main():
             time.sleep(5)
 
         time.sleep(60)  # Repeat every 60 seconds
+
 
 # Run the main function
 if __name__ == "__main__":
