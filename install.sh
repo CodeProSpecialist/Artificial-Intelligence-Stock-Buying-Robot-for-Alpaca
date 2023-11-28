51ae6b6150fd5e769e4f5038d9c286b1032df41c
codeprospecialist <droark28@hotmail.com>
2023-11-27 20:35:20 -0600

    Code bugs were fixed.
    Signed-off-by: codeprospecialist <droark28@hotmail.com>

--- install.sh
+++ install.sh
@@ -9,5 +9,7 @@ sudo apt-get install -y python3 python3-pip
 # Install required Python packages
 sudo pip3 install torch alpaca-trade-api yfinance transformers requests beautifulsoup4 
 
+transformers-cli download nlptown/bert-base-multilingual-uncased-sentiment
+
 # Install required system packages
 sudo apt-get install -y libopenblas-dev
